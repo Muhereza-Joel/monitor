c247ec75024223e4d84ccf499b488a498e1ccaf0
Muhereza Joel <muherezajoel40@gmail.com>
2024-07-02 19:01:54 +0300

    improved login ui

--- view/templates/auth/login.blade.php
+++ view/templates/auth/login.blade.php
@@ -2,68 +2,84 @@
 
 <body>
     <main>
-        <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-4 p-5 col-md-4 d-flex flex-column align-items-center justify-content-center">
-                        <div class="card">
-                            <div class="d-flex flex-column justify-content-center">
-                                <a href="/{{$appName}}" class="logo d-flex align-items-center w-auto">
-                                    <img src="/{{$appName}}/assets/img/logo_yellow.png" style="width: 300px; object-fit:contain;" alt="logo">
-                                </a>
-                              
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-4 p-5 col-md-4 d-flex flex-column align-items-center justify-content-center">
+                    <div class="card mt-4">
+                        <div class="d-flex flex-column justify-content-center">
+                            <a href="/{{$appName}}" class="logo d-flex align-items-center w-auto">
+                                <img src="/{{$appName}}/assets/img/logo_yellow.png" style="width: 300px; object-fit:contain;" alt="logo">
+                            </a>
+                        </div>
+                        <!-- End Logo -->
+                        <div class="card-body">
+                            <div class="pt-0 text-center">
+                                <h5 class="card-title pb-0 fs-4">{{$appNameFull}}</h5>
+                                <p class="small">Enter your username & password to login</p>
                             </div>
-                            <!-- End Logo -->
-                            <div class="card-body">
-                                <div class="pt-0 text-center">
-                                    <h5 class="card-title pb-0 fs-4">{{$appNameFull}}</h5>
-                                    <p class="small">Enter your username & password to login</p>
-                                </div>
 
-                                <div id="invalid-login" class="alert alert-danger alert-dismissible fade d-none p-1" role="alert">
-                                    <span class="text-center"></span>
-                                </div>
+                            <div id="invalid-login" class="alert alert-danger alert-dismissible fade d-none p-1" role="alert">
+                                <span class="text-center"></span>
+                            </div>
 
-                                <form id="login-form" class="row g-3 needs-validation" novalidate>
-                                    <div class="col-12">
-                                        <label for="yourUsername" class="form-label">Username or Email</label>
-                                        <div class="input-group has-validation">
-                                            <input type="text" name="username" class="form-control" id="yourUsername" required>
-                                            <div class="invalid-feedback">Please enter your username.</div>
-                                        </div>
+                            <form id="login-form" class="row g-3 needs-validation" novalidate>
+                                <div class="col-12">
+                                    <label for="yourUsername" class="form-label">Username or Email</label>
+                                    <div class="input-group has-validation">
+                                        <input type="text" name="username" class="form-control" id="yourUsername" required placeholder="Enter your username or email">
+                                        <div class="invalid-feedback">Please enter your username or email.</div>
                                     </div>
+                                </div>
 
-                                    <div class="col-12">
-                                        <label for="yourPassword" class="form-label">Password</label>
-                                        <input type="password" name="password" class="form-control" id="yourPassword" required>
+                                <div class="col-12">
+                                    <label for="yourPassword" class="form-label">Password</label>
+                                    <div class="input-group has-validation">
+                                        <div class="input-group">
+                                            <input type="password" name="password" class="form-control" id="yourPassword" required placeholder="Enter your password">
+                                            <button class="btn btn-secondary" type="button" id="togglePasswordVisibility" onclick="togglePasswordVisibility()"><i class="bi bi-eye"></i></button>
+                                        </div>
                                         <div class="invalid-feedback">Please enter your password!</div>
                                     </div>
+                                </div>
 
-                                    <div class="col-12">
-                                        <button id="login-button" class="btn btn-secondary w-100" type="submit">Sign in</button>
-                                    </div>
-                                    <div class="card-footer p-0">
-                                        <div class="col-12 text-center">
-                                            <p>
-                                                <a href="/{{$appName}}/auth/organizations/choose/">Create an account</a>|
-                                                <a href="/{{$appName}}/auth/accounts/reset/" class="text-danger ms-2">Forgot Password</a>
-                                            </p>
+                                <div class="col-12">
+                                    <button id="login-button" class="btn btn-secondary w-100" type="submit">Sign in</button>
+                                </div>
+                                <div class="card-footer p-0">
+                                    <div class="col-12 text-center">
+                                        <p>
+                                            <a href="/{{$appName}}/auth/organizations/choose/">Create an account</a>|
+                                            <a href="/{{$appName}}/auth/accounts/reset/" class="text-danger ms-2">Forgot Password</a>
+                                        </p>
 
-                                        </div>
                                     </div>
-                                </form>
-                            </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
     </main>
+
     <!-- End #main -->
     @include('partials/footer')
 
     <script>
         $(document).ready(function() {
+            $('#togglePasswordVisibility').click(function() {
+                let passwordInput = $('#yourPassword');
+                let passwordVisibilityIcon = $(this).find('i');
+
+                if (passwordInput.attr('type') === 'password') {
+                    passwordInput.attr('type', 'text');
+                    passwordVisibilityIcon.removeClass('bi-eye').addClass('bi-eye-slash');
+                } else {
+                    passwordInput.attr('type', 'password');
+                    passwordVisibilityIcon.removeClass('bi-eye-slash').addClass('bi-eye');
+                }
+            });
+
             $('#login-form').submit(function(e) {
                 e.preventDefault();
 
